898815de50ee5bed27e777f0185fa2b6d8cfcc4b
Mercutio3 <santiagodmzham@gmail.com>
2024-05-07 20:33:45 +0200

    testdbc updated to reflect dbc.py changes

--- sprint_2/wdc/tests/testdbc.py
+++ sprint_2/wdc/tests/testdbc.py
@@ -5,14 +5,14 @@ from dbc import DatabaseConnection
 
 # Very simple test class for the Database Connection object
 class TestDatabaseConnection(unittest.TestCase):
-    def setUp(self):
-        self.db_connection = DatabaseConnection()
-
     def test_initialization(self):
-        self.assertEqual(self.db_connection.WCPS_EndPoint, "https://ows.rasdaman.org/rasdaman/ows")
-        self.assertEqual(self.db_connection.serverOptions["service"], "WCS")
-        self.assertEqual(self.db_connection.serverOptions["version"], "2.1.0")
-        self.assertEqual(self.db_connection.base_wcs_url, "https://ows.rasdaman.org/rasdaman/ows?service=WCS&version=2.1.0")
+        # Set up a real DatabaseConnection instance
+        db_url = "https://ows.rasdaman.org/rasdaman/ows"
+        db_connection = DatabaseConnection(database_url=db_url)
+
+        # Check if DatabaseConnection is properly initialized
+        self.assertEqual(db_connection.WCPS_EndPoint, db_url)
+        self.assertEqual(db_connection.base_wcs_url, db_url + "?service=WCS&version=2.1.0")
 
 if __name__ == '__main__':
     unittest.main()
